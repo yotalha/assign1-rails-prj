42b1f95e2a19cf106505fa1be5d77da265c00bae
“yotalha” <talha.coder9@gmail.com>
2023-03-13 04:21:54 +0500

    ruby on rails section 4 completed - CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "pages#home"
   get 'test', to: 'pages#test'
-  resources :articles, only: [:show, :index]
+  resources :articles #,only: [:show, :index]
 end
